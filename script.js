fd0ee0668bc83d35629741de7840150db2f2e165
chinelon <72803021+chinelon@users.noreply.github.com>
2022-03-16 00:02:57 +0100

    rest

---
+++ script.js
@@ -0,0 +1,56 @@
+const change = document.getElementById("bagels");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
+const change1 = document.getElementById("bagels1");
+
+change1.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
+const change2 = document.getElementById("bagels2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
+const change3 = document.getElementById("bagels3");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
+const change4 = document.getElementById("bagels4");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
+const change5 = document.getElementById("bagels5");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
+const change6 = document.getElementById("bagels6");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
+const change7 = document.getElementById("bagels7");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
